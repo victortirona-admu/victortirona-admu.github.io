3dce145a3ccef45b8712b440326c1cd3e5e93e28
victortirona-admu <64826293+victortirona-admu@users.noreply.github.com>
2021-04-24 02:00:22 +0800

    Implementing Single Page Design with Scrolling
    I once again am redesigning the whole page because I wasn't satisfied with how it looked. The last design felt like I was cramming too much information in one screen. This time, I make sure to add a landing screen and add succeeding screens to tell me story. I think this is my final redesign for now.

---
+++ pages/2/script.js
@@ -0,0 +1,18 @@
+class Fortress{
+    constructor (level,capacity,rate){
+        this.level = level;
+        this.capacity = capacity;
+        this.rate = rate;
+    }
+}
+
+
+/*upgrade
+1. Upgrade
+2. Send over troops?
+3. Number of troops in a base also means lives of that base
+4. Base regenerates every few seconds
+5. Increasing level increases base capacity
+3. Build?
+
+*/
---
+++ script.js
@@ -0,0 +1,9 @@
+new TypeIt(".type-effect", {
+    startDelete: true,
+    strings: ["build and write code","organize teams","love like no other"],
+    breakLines: false,
+    nextStringDelay: 1250,
+    deleteSpeed: 50,
+    loop:true,
+    lifeLike: true
+  }).go();
